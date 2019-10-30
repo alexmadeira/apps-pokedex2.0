b2da0eba2ad354d57073228b41388f062b3751f8
AlexTemp <alextemp@MBP-de-D-CODE.americas.media.global.loc>
2019-10-30 16:31:00 -0300

    show pokemon (previous, current and next )

--- src/pages/Pokemon/index.js
+++ src/pages/Pokemon/index.js
@@ -1,12 +1,83 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 
-import { Container } from './styles';
+import api from '~/services/Api';
+import Title from '~/components/Title';
+import PokemonsImage from '~/assets/pokemons';
+
+import {
+  Container,
+  PageContent,
+  Region,
+  PokemonBox,
+  PokemonList,
+  PreviousPokemon,
+  CurrentPokemon,
+  NextPokemon,
+  HidePokemon,
+  ShowPokemon,
+} from './styles';
 
 function Pokemon({ match: { params } }) {
   const { slug } = params;
+  const [previousPokemon, setPreviousPokemon] = useState({});
+  const [currentPokemon, setCurrentPokemon] = useState({});
+  const [nextPokemon, setNextPokemon] = useState({});
 
-  return <Container>{slug}</Container>;
+  useEffect(() => {
+    const getPokemon = async () => {
+      const current = await api.get(`pokemon/${slug}`);
+      const previous = await api.get(`pokemon/${current.data.id - 1}`);
+      const next = await api.get(`pokemon/${current.data.id + 1}`);
+
+      setCurrentPokemon(current.data);
+      setPreviousPokemon(previous.data);
+      setNextPokemon(next.data);
+    };
+    getPokemon();
+  }, [slug]);
+
+  return (
+    <Container style={{ background: `#${slug}` }}>
+      <Title id={30} name={slug} />
+
+      <PageContent>
+        <Region>
+          <p>Region: Kanto</p>
+        </Region>
+        <PokemonBox>
+          <PokemonList>
+            <PreviousPokemon>
+              <HidePokemon src={PokemonsImage[previousPokemon.name]} />
+            </PreviousPokemon>
+            <CurrentPokemon>
+              <ShowPokemon src={PokemonsImage[currentPokemon.name]} />
+            </CurrentPokemon>
+            <NextPokemon>
+              <HidePokemon src={PokemonsImage[nextPokemon.name]} />
+            </NextPokemon>
+          </PokemonList>
+        </PokemonBox>
+        {/* <div>
+          <div>
+            <ul>
+              <li>Lorem</li>
+              <li>Lorem</li>
+            </ul>
+          </div>
+          <div>
+            <div>
+              <ul>
+                <li>Lorem:LoremLorem</li>
+                <li>Lorem:Lorem</li>
+                <li>Lorem:LoremLoremLorem</li>
+              </ul>
+            </div>
+          </div>
+        </div> */}
+      </PageContent>
+    </Container>
+  );
 }
 
 Pokemon.propTypes = {
--- src/pages/Pokemon/styles.js
+++ src/pages/Pokemon/styles.js
@@ -1,3 +1,89 @@
 import styled from 'styled-components';
+import { Colors, Spaces, Fonts } from '~/styles/Metrics';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  height: 100%;
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+`;
+
+export const PageContent = styled.div`
+  flex: 1;
+  height: 100%;
+  display: flex;
+  flex-direction: row;
+  position: absolute;
+  background: rgba(0, 0, 255, 0.1);
+  left: 0;
+  top: 0;
+  height: 100%;
+  width: 100%;
+`;
+
+export const Region = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-end;
+  justify-self: flex-end;
+  padding: ${Spaces.BasePadding};
+  z-index: 2;
+  p {
+    margin-bottom: calc(${Spaces.BaseMargin} * 5);
+    transform: rotate(-90deg);
+    transform-origin: left top;
+    font-size: ${Fonts.sizes.small};
+    color: ${Colors.White};
+    font-weight: bold;
+  }
+`;
+
+export const PokemonBox = styled.div`
+  left: 0;
+  top: 0;
+  height: 100%;
+  width: 100%;
+  position: absolute;
+`;
+
+export const PokemonList = styled.ul`
+  list-style: none;
+  display: flex;
+  flex: 1;
+  height: 100%;
+  /* width: 120vw; */
+  position: relative;
+  /* left: -10vw; */
+`;
+
+export const PreviousPokemon = styled.li`
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+export const CurrentPokemon = styled.li`
+  flex: 4;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+export const NextPokemon = styled.li`
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const HidePokemon = styled.img`
+  filter: brightness(0);
+  opacity: 0.1;
+  max-width: 100%;
+`;
+export const ShowPokemon = styled.img`
+  width: 70%;
+  opacity: 1;
+`;
