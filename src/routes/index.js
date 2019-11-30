363392b133ca59c155878f9c439fc9840b4657ab
Alex Madeira <alexmadeira@MacBook-Pro-de-Alex.local>
2019-11-30 18:50:11 -0300

    remove teste

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,13 +2,11 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import Pokedex from '~/pages/Pokedex';
-import Teste from '~/pages/Teste';
 
 export default function Routes() {
   return (
     <Switch>
       <Route path="/" exact component={Pokedex} />
-      <Route path="/teste" component={Teste} />
     </Switch>
   );
 }
